86ff523d078bee09b45d47770b6119bcbe0da75a
Budhathoki, Karan <budhathokikaran135@gmail.com>
2024-04-27 23:31:35 -0500

    creating dto for customer with error handling

---
+++ src/main/java/org/microservices/accounts/dto/AccountsDto.java
@@ -0,0 +1,10 @@
+package org.microservices.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+    private Long account_number;
+    private String account_type;
+    private String branch_address;
+}
